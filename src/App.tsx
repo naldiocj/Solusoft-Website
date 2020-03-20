eb18bc02dafa46da47fbe44619874f3a8350ed60
naldiocj <naldiocj@gmail.com>
2020-03-20 14:26:52 +0100

    Nova estrutura para o projecto

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,7 @@ import './styles.css'
 
 function App() {
   return (
-    <div className='container'>
-      <header className='d-flex justify-content-center'>
-        <img src={logo} alt="" />
-      </header>
-      <div className="horizontal-line"></div>
-      <main className='d-flex justify-content-center'>
-        <div className='text-center'>
-          <h5>(+244) 948145100 | geral@solusoft.co.ao</h5>
-          <h5>Talatona - Luanda, Angola</h5>
-        </div>
-      </main>
-      <footer>
-        <h1 className="text-center under-text">Under Review</h1>
-      </footer>
-    </div >
+    <div></div>
   );
 }
 
